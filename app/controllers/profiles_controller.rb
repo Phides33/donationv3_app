77142547fb59a87ff8080bd416dc00d6c9cd010d
Philippe Despreaux <despreaux.philippe@wanadoo.fr>
2018-01-05 17:09:18 +0100

    fonctionne pour l'ensemble du questionnaire

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,10 @@
 class ProfilesController < ApplicationController
   before_action :prof_find, only: [:edit, :update]
 
+  def index
+    @profile = Profile.last
+  end
+
   def new
     @profile = Profile.new
   end
@@ -22,22 +26,55 @@ class ProfilesController < ApplicationController
   end
 
   def update
-    if @profile.step == 3
-      @profile.full_name = pparams[:full_name]
-      @profile.save
-      flash.now[:alert] = "Succès!"
-    elsif @profile.step == 2
-      if pparams.values[0] != ""
-        a = pparams.keys[0].to_sym
-        @profile[a] = pparams.values[0].to_i
+    if @profile.step == 5 || @profile.step == 10
+      a = pparams.values[0]
+      k = pparams.keys[0].to_sym
+      if !!(a =~ /\A[^\/\\@]*\z/) && (a.length <= 200) && (a.length >= 3)
+        @profile[k] = a
+        @profile.step += 1
+        @profile.save
+        redirect_to edit_profile_path(@profile)
+      elsif !(a =~ /\A[^\/\\@]*\z/)
+        flash.now[:alert] = "Votre réponse ne peut pas comporter des caractères spéciaux"
+        render :edit
+      elsif a.length > 200
+        flash.now[:alert] = "Votre réponse est un peu longue, non ?"
+        render :edit
+      else
+        flash.now[:alert] = "Votre réponse est un peu courte, non ?"
+        render :edit
+      end
+    elsif @profile.step == 7
+      if !!(pparams[:po_code] =~ /^\d{5}$/)
+        @profile.po_code = pparams[:po_code]
         @profile.step += 1
         @profile.save
         redirect_to edit_profile_path(@profile)
       else
-        flash.now[:alert] = "Veuillez sélectionner une des options proposées"
+        flash.now[:alert] = "Votre réponse doit comporter 5 chiffres exactement"
         render :edit
       end
-    elsif @profile.step == 1
+    elsif @profile.step == 11
+      if !!(pparams[:mobile_number] =~ /^0[6-7][0-9]{8}$/)
+        @profile.mobile_number = pparams[:mobile_number]
+        @profile.step += 1
+        @profile.save
+        redirect_to edit_profile_path(@profile)
+      else
+        flash.now[:alert] = "Votre réponse doit comporter 10 chiffres exactement,
+          le premier doit être 0 et le second 6 ou 7"
+        render :edit
+      end
+    elsif @profile.step == 12
+      if !!(pparams[:email] =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/)
+        @profile.email = pparams[:email]
+        @profile.save
+        redirect_to profiles_path(@profile)
+      else
+        flash.now[:alert] = "Votre réponse doit être un email valide"
+        render :edit
+      end
+    else
       if pparams.values[0] != ""
         a = pparams.keys[0].to_sym
         @profile[a] = pparams.values[0].to_i
@@ -58,6 +95,8 @@ class ProfilesController < ApplicationController
   end
 
   def pparams
-    params.require(:profile).permit(:full_name, :amount_bracket, :cause, :own_emotion)
+    params.require(:profile).permit(:full_name, :amount_bracket, :cause, :own_emotion,
+      :other_emotion, :preferred_mean, :money_wish, :age_bracket, :po_code,
+      :main_mobility_mode, :motor_vehicle_owner, :mobile_number, :email)
   end
 end
